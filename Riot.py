8ca07bfa61a7f2ba931b261c1063e94dd1655a76
VizualHealing <67212268+VizualHealing@users.noreply.github.com>
2020-07-10 14:24:19 -0400

    Moved to ddragon db, updated methods
    Removing Champion dir for champion.json from ddragon, fixed problem with createchamps method where it only added one champion to ChampList, removed redundant maxitems method and implemented it within additems method, created addlevel method.

--- Riot.py
+++ Riot.py
@@ -1,16 +1,9 @@
 import json
-import os
 ItemList = []
 ChampList = []
 
-myDir = os.getcwd() + '\Champions 10.8.1'
-
-
-
-
 
 class Champion:
-
     def __init__(self, name, stats):
         self.name = name
         self.items = []
@@ -23,24 +16,32 @@ class Champion:
     def getstats(self):
         return self.stats
 
-    def additems(self, item):
-        with open("StatMap.json", "r") as j:
-            data = json.load(j)
-        for thing in ItemList:
-            if thing.getitemname() == item:  # and (self.maxItems == False):
-                self.items.append(thing)
-                for ItemStatKey in thing.stats.keys():
-                    for ChampStatKey, ChampStat in self.stats.items():  # iterate the stat that each item gives
-                        if ChampStatKey == data["stats"][ItemStatKey]:
-                            self.stats[ChampStatKey] += thing.stats[ItemStatKey]
-                            break
+    def additems(self, additem):
+        if len(self.items) < 6:
+            with open("StatMap.json", "r") as j:
+                data = json.load(j)
+            for item in ItemList:
+                if item.getitemname() == additem:  # and (self.maxItems == False):
+                    self.items.append(item)
+                    for ItemStatKey in item.stats.keys():
+                        for ChampStatKey, ChampStat in self.stats.items():  # iterate the stat that each item gives
+                            if ChampStatKey == data["stats"][ItemStatKey]:
+                                self.stats[ChampStatKey] += item.stats[ItemStatKey]
+                                break
+
+    def addlevel(self):
+        self.stats["hp"] += self.stats["hpperlevel"]
+        self.stats["mp"] += self.stats["mpperlevel"]
+        self.stats["armor"] += self.stats["armorperlevel"]
+        self.stats["spellblock"] += self.stats["spellblockperlevel"]
+        self.stats["hpregen"] += self.stats["hpregenperlevel"]
+        self.stats["mpregen"] += self.stats["mpregenperlevel"]
+        self.stats["crit"] += self.stats["critperlevel"]
+        self.stats["attackdamage"] += self.stats["attackdamageperlevel"]
+        self.stats["attackspeed"] += self.stats["attackspeedperlevel"]
 
     def getitemlist(self):
         return self.items
-
-    def maxitems(self):  # Set the max amount of Items that any champion can have
-        if len(self.items) == 6:
-            self.maxItems = True
 
 
 class Items:
@@ -75,12 +76,11 @@ def readitems():
 
 
 def createchamps():
-    thisdir = os.getcwd() + '/Champions 10.8.1'
-    for file in os.listdir(thisdir):
-        with open("Champions 10.8.1/" + file, "r", encoding="mbcs") as j:
-            data = json.load(j)
-            champ = Champion(file[:-5], data["data"]["stats"])
-        ChampList.append(champ)
+    with open("champion.json", "r", encoding="utf8") as j:
+        data = json.load(j)
+        for key in data["data"]:
+            champ = Champion(data["data"][key], data["data"][key]["stats"])
+            ChampList.append(champ)
 
 
 def createitems():
@@ -97,26 +97,22 @@ def searchitems(itemname, itemlist):
         if obj.getitemname() == itemname:
             return obj
 
+
 def searchchamp(champname, champlist):
     for obj in champlist:
         if obj.getchamp() == champname:
             return obj
 
 
-
 def compare(champ1, champ2):
     pass
 
 createchamps()
-
 createitems()
 print(ChampList[0].getstats())
-ChampList[0].additems("Pickaxe")
+# ChampList[0].additems("Pickaxe")
+Champion.addlevel(ChampList[0])
 print(ChampList[0].getstats())
 
 
 #print(searchchamp("Aatrox", ChampList))
-
-
-
-
